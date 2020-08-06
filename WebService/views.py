da740e4e4bc43c0350a005447acdb932d6d0112e
EvanAbra <1627352666@qq.com>
2020-08-06 17:32:14 +0800

    [bugfix]修复博客中部分文章及图片无法访问的问题

--- WebService/views.py
+++ WebService/views.py
@@ -3,8 +3,19 @@ Author: Baixu
 Date: 2020-07-10
 Desc: 网站主入口的视图函数
 """
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def redirect(request, path):
+    """
+    所有url均匹配失效时调用该函数，通知用户重定向至对应的https链接再尝试一次
+    :param path: 用户提交的url
+    :param request: WSGI对象，用户的这次网络请求
+    :return: 重定向的回复
+    """
+    return HttpResponseRedirect('https://7venminutes.com/' + path)
